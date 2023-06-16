6c996d698bdcf5bdab5c4a55477a12a866ed73ea
Liam Laverty <liamlaverty@gmail.com>
2023-06-16 16:28:19 +0100

    adds more detail to missing envvar exceptions

--- src/EnvVarValidator.ts
+++ src/EnvVarValidator.ts
@@ -1,3 +1,5 @@
+import { error } from "console";
+
 export class EnvVarValidator{
     /* 
     *   Verifies the process env vars are all set in this environment, 
@@ -6,9 +8,6 @@ export class EnvVarValidator{
     public static VerifyEnvVars(): void {
         const envVarKeys = [
             'DATA_PACKAGE_FILE_PATH',
-            // 'POSTGRES_USER',
-            // 'POSTGRES_PASSWORD',
-            // 'POSTGRES_DB',
             'PGHOST',
             'PGUSER',
             'PGDATABASE',
@@ -16,11 +15,23 @@ export class EnvVarValidator{
             'PGPORT'
         ];
 
+        const errors = [];
         for (let i = 0; i < envVarKeys.length; i++) {
             const pEnvVar = process.env[envVarKeys[i]];
             if (pEnvVar === null || pEnvVar === undefined || pEnvVar.length === 0) {
-                throw new Error(`Process env var '${envVarKeys[i]}' was null or undefined. Set inside the file '.env' in the root of this project`);
+                errors.push(`Process env var '${envVarKeys[i]}' was null or undefined. Set inside the file '.env' in the root of this project`)
+
             }
+        }
+
+        if (errors.length > 0) {
+            let errMessage = `${errors.length} errors in envvars. Errors:`;
+            let idx = 0;
+            errors.forEach(thisErr => {
+                errMessage += ` |Err ${idx}: ${thisErr}`;
+                idx++;
+            });
+            throw new Error(errMessage)
         }
         
         console.log('all env vars were set correctly');
